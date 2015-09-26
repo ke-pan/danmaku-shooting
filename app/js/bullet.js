4468756fa16381a4d10de105c558334549592e37
ke-pan <ke_pan@yeah.net>
2015-09-26 19:51:51 +0800

    model bullet

---
+++ app/js/bullet.js
@@ -0,0 +1,45 @@
+function Bullet() {
+    this.image = image
+    this.context = context
+    this.width = image.width
+    this.height = image.height
+    this.x = 0
+    this.y = 0
+    this.speed = 10
+    this.alive = false
+}
+
+Ship.prototype.draw = function() {
+    this.context.drawImage(this.image, this.x, this.y)
+};
+
+Ship.prototype.clear = function() {
+    this.context.clearRect(this.x, this.y, this.width, this.height)
+};
+
+Bullet.prototype.init = function(x, y) {
+    this.x = x
+    this.y = y
+    this.alive = true
+    this.fly()
+};
+
+Bullet.prototype.reset = function() {
+    this.x = 0
+    this.y = 0
+    this.alive = false
+};
+
+Bullet.prototype.fly = function() {
+    this.clear()
+    this.x = this.x + this.speed
+    if (this.x > this.context.canvas.width) {
+        this.reset()
+    }
+    else {
+        this.draw()
+    }
+    if (this.alive) {
+        requestAnimationFrame(this.fly.bind(this))
+    }
+}
